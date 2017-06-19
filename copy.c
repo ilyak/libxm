da79b25db6975794931a7f70839c0cf273c4dffc
Ilya Kaliman <ilya.kaliman@gmail.com>
2017-06-19 14:43:26 -0700

    Remove unneeded includes.

--- copy.c
+++ copy.c
@@ -14,10 +14,7 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#include <stdarg.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #include "xm.h"
 #include "util.h"
