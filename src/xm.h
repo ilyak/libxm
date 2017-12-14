a2d01d61247409f0e5ced656503771a9bd4105ef
Ilya Kaliman <ilya.kaliman@gmail.com>
2017-12-14 13:15:29 -0800

    Doxygenify docs.

--- src/xm.h
+++ src/xm.h
@@ -19,63 +19,65 @@
 
 #include "tensor.h"
 
+/** \file */
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/* Print libxm banner to the standard output. */
+/** Print libxm banner to the standard output. */
 void xm_print_banner(void);
 
-/* Set all canonical block elements of tensor "a" to value "x". */
+/** Set all canonical block elements of tensor "a" to value "x". */
 void xm_set(xm_tensor_t *a, xm_scalar_t x);
 
-/* Copy tensor block data from "b" to "a" while multiplying by a scaling factor
- * (a = s * b). Tensors must have compatible block-structures. Permutations are
- * specified using strings idxa and idxb. This function does not change the
- * original block-structure of the output tensor.
+/** Copy tensor block data from "b" to "a" while multiplying by a scaling factor
+ *  (a = s * b). Tensors must have compatible block-structures. Permutations are
+ *  specified using strings idxa and idxb. This function does not change the
+ *  original block-structure of the output tensor.
  *
- * Example: xm_copy(a, 2.0, b, "ijk", "kij");
- *          a_ijk = 2 * b_kij
+ *  Example: xm_copy(a, 2.0, b, "ijk", "kij");
+ *           a_ijk = 2 * b_kij
  */
 void xm_copy(xm_tensor_t *a, xm_scalar_t s, const xm_tensor_t *b,
     const char *idxa, const char *idxb);
 
-/* Add tensors (a = alpha * a + beta * b). Tensors must have compatible
- * block-structures. Permutations are specified using strings idxa and idxb.
- * This function does not change the original block-structure of the output
- * tensor.
+/** Add tensors (a = alpha * a + beta * b). Tensors must have compatible
+ *  block-structures. Permutations are specified using strings idxa and idxb.
+ *  This function does not change the original block-structure of the output
+ *  tensor.
  *
- * Example: xm_add(1.0, a, 2.0, b, "ij", "ji");
- *          a_ij = a_ij + 2 * b_ji
+ *  Example: xm_add(1.0, a, 2.0, b, "ij", "ji");
+ *           a_ij = a_ij + 2 * b_ji
  */
 void xm_add(xm_scalar_t alpha, xm_tensor_t *a, xm_scalar_t beta,
     const xm_tensor_t *b, const char *idxa, const char *idxb);
 
-/* Divide tensor elements. Tensors must have compatible block-structures.
- * Permutations are specified using strings idxa and idxb. This function does
- * not change the original block-structure of the output tensor.
+/** Divide tensor elements. Tensors must have compatible block-structures.
+ *  Permutations are specified using strings idxa and idxb. This function does
+ *  not change the original block-structure of the output tensor.
  *
- * Example: xm_div(a, b, "ij", "ji");
- *          a_ij = a_ij / b_ji
+ *  Example: xm_div(a, b, "ij", "ji");
+ *           a_ij = a_ij / b_ji
  */
 void xm_div(xm_tensor_t *a, const xm_tensor_t *b, const char *idxa,
     const char *idxb);
 
-/* Dot product of two tensors. Tensors must have compatible block-structures.
- * Permutations are specified using strings idxa and idxb. */
+/** Dot product of two tensors. Tensors must have compatible block-structures.
+ *  Permutations are specified using strings idxa and idxb. */
 xm_scalar_t xm_dot(const xm_tensor_t *a, const xm_tensor_t *b,
     const char *idxa, const char *idxb);
 
-/* Contract two tensors over contraction indices specified by strings idxa and
- * idxb (c = alpha * a * b + beta * c). Permutation of tensor "c" is specified
- * by idxc. The routine will perform optimal contraction using symmetry and
- * sparsity information obtained from tensors' block-structures. It is the
- * user's responsibility to setup all tensors so that they have correct
- * symmetries. This function does not change the original block-structure of
- * the output tensor.
+/** Contract two tensors over contraction indices specified by strings idxa and
+ *  idxb (c = alpha * a * b + beta * c). Permutation of tensor "c" is specified
+ *  by idxc. The routine will perform optimal contraction using symmetry and
+ *  sparsity information obtained from tensors' block-structures. It is the
+ *  user's responsibility to setup all tensors so that they have correct
+ *  symmetries. This function does not change the original block-structure of
+ *  the output tensor.
  *
- * Example: xm_contract(1.0, a, b, 2.0, c, "abcd", "ijcd", "ijab");
- *          c_ijab = a_abcd * b_ijcd + 2 * c_ijab
+ *  Example: xm_contract(1.0, a, b, 2.0, c, "abcd", "ijcd", "ijab");
+ *           c_ijab = a_abcd * b_ijcd + 2 * c_ijab
  */
 void xm_contract(xm_scalar_t alpha, const xm_tensor_t *a, const xm_tensor_t *b,
     xm_scalar_t beta, xm_tensor_t *c, const char *idxa, const char *idxb,
