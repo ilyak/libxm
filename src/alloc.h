db13af824e100616125863993819f05a74741135
Ilya Kaliman <ilya.kaliman@gmail.com>
2017-12-18 11:22:45 -0800

    Tag parameter references.

--- src/alloc.h
+++ src/alloc.h
@@ -33,7 +33,7 @@ extern "C" {
 typedef struct xm_allocator xm_allocator_t;
 
 /** Create a disk-backed allocator. The file specified by \p path will be
- *  created and used by the allocator for data storage. If path is NULL, all
+ *  created and used by the allocator for data storage. If \p path is NULL, all
  *  data will be allocated from RAM.
  *  \param path Path to file backing the allocator.
  *  \return New instance of ::xm_allocator_t. */
@@ -44,16 +44,16 @@ xm_allocator_t *xm_allocator_create(const char *path);
  *  \return File path. */
 const char *xm_allocator_get_path(xm_allocator_t *allocator);
 
-/** Allocate storage of the specified size from this allocator. This returns
- *  a data_ptr handle which is used by other allocator functions.
+/** Allocate storage of the specified size from this allocator. This function
+ *  returns \p data_ptr handle which is used by other allocator functions.
  *  \param allocator An allocator.
  *  \param size_bytes Size of the allocation in bytes.
  *  \return Virtual pointer to the allocated data. */
 uint64_t xm_allocator_allocate(xm_allocator_t *allocator,
     size_t size_bytes);
 
-/** Read data from the data_ptr into memory. The size argument must match the
- *  size of the corresponding allocation.
+/** Read data from the \p data_ptr into memory. The size argument must match
+ *  the size of the corresponding allocation.
  *  \param allocator An allocator.
  *  \param data_ptr Data pointer.
  *  \param mem Pointer to memory.
@@ -61,8 +61,8 @@ uint64_t xm_allocator_allocate(xm_allocator_t *allocator,
 void xm_allocator_read(xm_allocator_t *allocator, uint64_t data_ptr,
     void *mem, size_t size_bytes);
 
-/** Write data from memory into the data_ptr. The size argument must match the
- *  size of the corresponding allocation.
+/** Write data from memory into the \p data_ptr. The size argument must match
+ *  the size of the corresponding allocation.
  *  \param allocator An allocator.
  *  \param data_ptr Data pointer.
  *  \param mem Pointer to memory.
@@ -70,7 +70,7 @@ void xm_allocator_read(xm_allocator_t *allocator, uint64_t data_ptr,
 void xm_allocator_write(xm_allocator_t *allocator, uint64_t data_ptr,
     const void *mem, size_t size_bytes);
 
-/** Deallocate data pointed to by the data_ptr.
+/** Deallocate data pointed to by the \p data_ptr.
  *  \param allocator An allocator.
  *  \param data_ptr Pointer to deallocate. */
 void xm_allocator_deallocate(xm_allocator_t *allocator, uint64_t data_ptr);
