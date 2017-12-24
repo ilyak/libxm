8d4236796dd9285186e2e3503aec4483cd531c67
Ilya Kaliman <ilya.kaliman@gmail.com>
2017-12-24 13:01:37 -0800

    Documentation.

--- src/dim.h
+++ src/dim.h
@@ -38,31 +38,31 @@ typedef struct {
 
 /** Return new ::xm_dim_t with all indices initialized to zero.
  *  \param ndim Number of dimensions in the returned dim.
- *  \return New ::xm_dim_t. */
+ *  \return New instance of ::xm_dim_t. */
 xm_dim_t xm_dim_zero(size_t ndim);
 
 /** Return new ::xm_dim_t with all indices initialized to the same value.
  *  \param ndim Number of dimensions in the returned dim.
  *  \param dim Size of the dimensions.
- *  \return New ::xm_dim_t. */
+ *  \return New instance of ::xm_dim_t. */
 xm_dim_t xm_dim_same(size_t ndim, size_t dim);
 
 /** Initialize a 1-D dim.
  *  \param dim1 Dimension size.
- *  \return New ::xm_dim_t. */
+ *  \return New instance of ::xm_dim_t. */
 xm_dim_t xm_dim_1(size_t dim1);
 
 /** Initialize a 2-D dim.
  *  \param dim1 Dimension size.
  *  \param dim2 Dimension size.
- *  \return New ::xm_dim_t. */
+ *  \return New instance of ::xm_dim_t. */
 xm_dim_t xm_dim_2(size_t dim1, size_t dim2);
 
 /** Initialize a 3-D dim.
  *  \param dim1 Dimension size.
  *  \param dim2 Dimension size.
  *  \param dim3 Dimension size.
- *  \return New ::xm_dim_t. */
+ *  \return New instance of ::xm_dim_t. */
 xm_dim_t xm_dim_3(size_t dim1, size_t dim2, size_t dim3);
 
 /** Initialize a 4-D dim.
@@ -70,7 +70,7 @@ xm_dim_t xm_dim_3(size_t dim1, size_t dim2, size_t dim3);
  *  \param dim2 Dimension size.
  *  \param dim3 Dimension size.
  *  \param dim4 Dimension size.
- *  \return New ::xm_dim_t. */
+ *  \return New instance of ::xm_dim_t. */
 xm_dim_t xm_dim_4(size_t dim1, size_t dim2, size_t dim3, size_t dim4);
 
 /** Initialize a 5-D dim.
@@ -79,7 +79,7 @@ xm_dim_t xm_dim_4(size_t dim1, size_t dim2, size_t dim3, size_t dim4);
  *  \param dim3 Dimension size.
  *  \param dim4 Dimension size.
  *  \param dim5 Dimension size.
- *  \return New ::xm_dim_t. */
+ *  \return New instance of ::xm_dim_t. */
 xm_dim_t xm_dim_5(size_t dim1, size_t dim2, size_t dim3, size_t dim4,
     size_t dim5);
 
@@ -90,7 +90,7 @@ xm_dim_t xm_dim_5(size_t dim1, size_t dim2, size_t dim3, size_t dim4,
  *  \param dim4 Dimension size.
  *  \param dim5 Dimension size.
  *  \param dim6 Dimension size.
- *  \return New ::xm_dim_t. */
+ *  \return New instance of ::xm_dim_t. */
 xm_dim_t xm_dim_6(size_t dim1, size_t dim2, size_t dim3, size_t dim4,
     size_t dim5, size_t dim6);
 
@@ -102,7 +102,7 @@ xm_dim_t xm_dim_6(size_t dim1, size_t dim2, size_t dim3, size_t dim4,
  *  \param dim5 Dimension size.
  *  \param dim6 Dimension size.
  *  \param dim7 Dimension size.
- *  \return New ::xm_dim_t. */
+ *  \return New instance of ::xm_dim_t. */
 xm_dim_t xm_dim_7(size_t dim1, size_t dim2, size_t dim3, size_t dim4,
     size_t dim5, size_t dim6, size_t dim7);
 
@@ -115,7 +115,7 @@ xm_dim_t xm_dim_7(size_t dim1, size_t dim2, size_t dim3, size_t dim4,
  *  \param dim6 Dimension size.
  *  \param dim7 Dimension size.
  *  \param dim8 Dimension size.
- *  \return New ::xm_dim_t. */
+ *  \return New instance of ::xm_dim_t. */
 xm_dim_t xm_dim_8(size_t dim1, size_t dim2, size_t dim3, size_t dim4,
     size_t dim5, size_t dim6, size_t dim7, size_t dim8);
 
@@ -131,40 +131,70 @@ int xm_dim_eq(const xm_dim_t *a, const xm_dim_t *b);
  *  \return Non-zero if the dims are NOT equal. */
 int xm_dim_ne(const xm_dim_t *a, const xm_dim_t *b);
 
-/** Return non-zero if an index is within zero and dim. */
+/** Return non-zero if an index is within zero-index and \p dim.
+ *  \param idx An index to test.
+ *  \param dim Maximum dimensions.
+ *  \return Non-zero if the index is within zero-index and \p dim. */
 int xm_dim_less(const xm_dim_t *idx, const xm_dim_t *dim);
 
-/** Return product of all indices of a dim. */
+/** Return product of the indices of a \p dim.
+ *  \param dim A dim.
+ *  \return Product of the indices of a \p dim. */
 size_t xm_dim_dot(const xm_dim_t *dim);
 
-/** Return absolute offset of an index in a dim. */
+/** Return absolute offset of an index.
+ *  \param idx An index within \p dim.
+ *  \param dim A dim.
+ *  \return Absolute offset of an index. */
 size_t xm_dim_offset(const xm_dim_t *idx, const xm_dim_t *dim);
 
-/** Compute an index from an offset in the specified dim. */
+/** Compute an index from an offset.
+ *  \param offset An absolute offset of an index.
+ *  \param dim A dim.
+ *  \return An index with the specified absolute offset. */
 xm_dim_t xm_dim_from_offset(size_t offset, const xm_dim_t *dim);
 
 /** Increment an index by one wrapping on dimensions. Can be used to iterate
- *  over all elements of a dim. */
+ *  over all elements of a dim.
+ *  \param idx An index.
+ *  \param dim Maximum dimensions. */
 void xm_dim_inc(xm_dim_t *idx, const xm_dim_t *dim);
 
-/** Set to zero all indices of this dim specified by mask. */
+/** Set to zero all indices of a dim according to the mask.
+ *  \param dim A dim.
+ *  \param mask Mask specifying affected indices. */
 void xm_dim_zero_mask(xm_dim_t *dim, const xm_dim_t *mask);
 
-/** Set maska indices of "a" to maskb indices of "b". */
+/** Set \p maska indices of \p a to \p maskb indices of \p b.
+ *  \param a Destination dim.
+ *  \param maska Mask of \p a.
+ *  \param b Source dim.
+ *  \param maskb Mask of \p b. */
 void xm_dim_set_mask(xm_dim_t *a, const xm_dim_t *maska, const xm_dim_t *b,
     const xm_dim_t *maskb);
 
-/** Return product of indices specified by mask. */
+/** Return product of indices specified by mask.
+ *  \param dim A dim.
+ *  \param mask Mask specifying affected indices.
+ *  \return Product of indices. */
 size_t xm_dim_dot_mask(const xm_dim_t *dim, const xm_dim_t *mask);
 
-/** Increment an index. This is similar to xm_dim_inc, but affects only indices
- *  specified by mask. */
+/** Increment an index. This is similar to ::xm_dim_inc function, but affects
+ *  only indices specified by \p mask.
+ *  \param idx An index to increment.
+ *  \param dim Maximum dimensions.
+ *  \param mask Mask specifying affected indices. */
 void xm_dim_inc_mask(xm_dim_t *idx, const xm_dim_t *dim, const xm_dim_t *mask);
 
-/** Return n-dimensional identity permutation. */
+/** Return an identity permutation.
+ *  \param ndim Dimensionality of the permutation.
+ *  \return A new identity permutation. */
 xm_dim_t xm_dim_identity_permutation(size_t ndim);
 
-/** Return a specific permutation of an index. */
+/** Return permutation of an index.
+ *  \param idx An index.
+ *  \param permutation Permutation.
+ *  \return Index with elements permuted according to the \p permutation. */
 xm_dim_t xm_dim_permute(const xm_dim_t *idx, const xm_dim_t *permutation);
 
 #ifdef __cplusplus
