77dee814857dd9e7ee6f0ddeb226241ddad18a22
Ilya Kaliman <ilya.kaliman@gmail.com>
2018-02-15 16:02:17 -0800

    Allow different tensor scalar types for xm_copy.

--- src/xm.c
+++ src/xm.c
@@ -67,7 +67,92 @@ void
 xm_copy(xm_tensor_t *a, xm_scalar_t s, const xm_tensor_t *b, const char *idxa,
     const char *idxb)
 {
-	xm_add(0, a, s, b, idxa, idxb);
+	const xm_block_space_t *bsa, *bsb;
+	xm_dim_t cidxa, cidxb, zero, *blklist;
+	xm_scalar_type_t scalartypea, scalartypeb;
+	size_t i, maxblkbytesa, maxblkbytesb, nblklist;
+	int mpirank = 0, mpisize = 1;
+
+	if (xm_tensor_get_allocator(a) != xm_tensor_get_allocator(b))
+		fatal("tensors must use same allocator");
+#ifdef XM_USE_MPI
+	MPI_Comm_rank(MPI_COMM_WORLD, &mpirank);
+	MPI_Comm_size(MPI_COMM_WORLD, &mpisize);
+#endif
+	bsa = xm_tensor_get_block_space(a);
+	bsb = xm_tensor_get_block_space(b);
+	if (strlen(idxa) != xm_block_space_get_ndims(bsa))
+		fatal("idxa does not match tensor dimensions");
+	if (strlen(idxb) != xm_block_space_get_ndims(bsb))
+		fatal("idxb does not match tensor dimensions");
+	xm_make_masks(idxa, idxb, &cidxa, &cidxb);
+	if (cidxa.n != xm_block_space_get_ndims(bsa) ||
+	    cidxb.n != xm_block_space_get_ndims(bsb))
+		fatal("index spaces do not match");
+	for (i = 0; i < cidxa.n; i++)
+		if (!xm_block_space_eq1(bsa, cidxa.i[i], bsb, cidxb.i[i]))
+			fatal("inconsistent block-spaces");
+
+	scalartypea = xm_tensor_get_scalar_type(a);
+	scalartypeb = xm_tensor_get_scalar_type(b);
+	zero = xm_dim_zero(0);
+	maxblkbytesa = xm_tensor_get_largest_block_bytes(a);
+	maxblkbytesb = xm_tensor_get_largest_block_bytes(b);
+	xm_tensor_get_canonical_block_list(a, &blklist, &nblklist);
+#ifdef _OPENMP
+#pragma omp parallel private(i)
+#endif
+{
+	xm_dim_t ia, ib;
+	void *buf1a, *buf2a, *buf1b, *buf2b;
+	size_t blksize;
+	int blocktype;
+
+	if ((buf1a = malloc(maxblkbytesa)) == NULL)
+		fatal("out of memory");
+	if ((buf2a = malloc(maxblkbytesa)) == NULL)
+		fatal("out of memory");
+	if ((buf1b = malloc(maxblkbytesb)) == NULL)
+		fatal("out of memory");
+	if ((buf2b = malloc(maxblkbytesb)) == NULL)
+		fatal("out of memory");
+	ib = xm_dim_zero(cidxb.n);
+#ifdef _OPENMP
+#pragma omp for schedule(dynamic)
+#endif
+	for (i = 0; i < nblklist; i++) {
+		if ((int)i % mpisize == mpirank) {
+			ia = blklist[i];
+			xm_dim_set_mask(&ib, &cidxb, &ia, &cidxa);
+			blksize = xm_tensor_get_block_size(b, ib);
+			blocktype = xm_tensor_get_block_type(b, ib);
+			if (s == 0 || blocktype == XM_BLOCK_TYPE_ZERO) {
+				memset(buf2a, 0, maxblkbytesa);
+			} else {
+				xm_scalar_t scalar = s;
+				scalar *= xm_tensor_get_block_scalar(b, ib);
+				xm_tensor_read_block(b, ib, buf2b);
+				xm_tensor_unfold_block(b, ib, cidxb, zero,
+				    buf2b, buf1b, blksize);
+				xm_scalar_mul(buf1b, scalar, blksize,
+				    scalartypeb);
+				xm_scalar_convert(buf1a, buf1b, blksize,
+				    scalartypea, scalartypeb);
+				xm_tensor_fold_block(a, ia, cidxa, zero, buf1a,
+				    buf2a, blksize);
+			}
+			xm_tensor_write_block(a, ia, buf2a);
+		}
+	}
+	free(buf1a);
+	free(buf2a);
+	free(buf1b);
+	free(buf2b);
+}
+	free(blklist);
+#ifdef XM_USE_MPI
+	MPI_Barrier(MPI_COMM_WORLD);
+#endif
 }
 
 void
